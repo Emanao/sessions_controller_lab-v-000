665ca034564cb582b0ef31c200d35139539224fa
Eva Navarro <emanao@me.com>
2020-04-25 16:04:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :logout
   def login
-    session
+    session[:name] ||= []
     session[:name]=params[:name]
   end
   def logout
